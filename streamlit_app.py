432b468c585788a30512797f83061806f59c1000
huwrees <h.w.rees@hotmail.co.uk>
2023-04-25 10:20:19 +0100

    Update streamlit_app.py
    comment out raw json
    output normalised json view in a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json()) # just writes the data to the screen 
+
+# takes the json version of the response and normalise it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it as a table
+streamlit.dataframe(fruityvice_normalized)
